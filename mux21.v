6ebdee792075485ad866c2d062b2eb3e22d22253
Seniru Dissanayake <81362941+SeniruD@users.noreply.github.com>
2023-02-10 22:18:15 +0530

    Update mux21.v

--- mux21.v
+++ mux21.v
@@ -1,5 +1,5 @@
 `timescale 1ns / 1ps
 
-module MUX(input [31:0] D0, input [31:0] D1, input select, output [31:0] SelData);
+module mux21(input [31:0] D0, input [31:0] D1, input select, output [31:0] SelData);
     assign SelData = (select == 0) ? D0 : D1;
 endmodule
